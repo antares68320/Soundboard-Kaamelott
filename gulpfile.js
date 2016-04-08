1a43279ed010e33817c996fd1a96538efa630933
Oprax <oprax@me.com>
2016-04-08 07:20:26 +0200

    clean + add jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     webserver = require('gulp-server-livereload'),
     useref = require('gulp-useref'),
+    jade = require('gulp-jade'),
     uglify = require('gulp-uglify'),
     clean = require('gulp-clean'),
     gulpif = require('gulp-if'),
@@ -15,11 +16,21 @@ gulp.task('img', function() {
 	  .pipe(gulp.dest('dist/img'));
 });
 
+gulp.task('jade', function () {
+	return gulp.src('src/*.jade')
+	  .pipe(jade({
+		locals: {},
+		pretty: true,
+		debug: true
+	  }))
+	  .pipe(gulp.dest('./src'));
+});
+
 gulp.task('clean', function () {
 	return gulp.src('dist', { read: false }).pipe(clean())
 });
 
-gulp.task('default', ['img', 'clean'], function() {
+gulp.task('default', ['jade', 'img', 'clean'], function() {
 	return gulp.src('src/*.html')
 	  .pipe(useref())
 	  .pipe(gulpif('*.js', uglify()))
@@ -27,8 +38,10 @@ gulp.task('default', ['img', 'clean'], function() {
 	  .pipe(gulp.dest('dist'));
 });
 
-gulp.task('serve', function() {
-	return gulp.src('./src')
+gulp.task('serve', ['jade'], function() {
+	gulp.watch('src/*.jade', ['jade']);
+	
+	gulp.src('./src')
 	  .pipe(webserver({
 		livereload: true,
 		directoryListing: false,
